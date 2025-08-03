69044266766e36a1dd7790da566cfd81683d782d
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-08-03 15:36:06 +0530

    Add files via upload

---
+++ oddEven.java
@@ -0,0 +1,14 @@
+package javaPrograms;
+
+public class oddEven {
+    public static void main(String[] args) {
+        int x = 2 ;
+        if (x%2 == 0){
+            System.out.println("u are smart");
+        }else{
+            System.err.println("u are dumb");
+        }
+        System.out.println("You are precious");
+
+    }
+}
