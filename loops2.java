e2c6a3a962bef19db6a40ef3ea389edff4fc76a2
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-08-08 07:22:27 +0530

    loops2.java with nested loop examples
    Includes Java programs using nested loops for pattern printing and logic building.
    Part of My100daysCodeChallenge to practice loop control and output formatting.

---
+++ loops2.java
@@ -0,0 +1,11 @@
+package javaPrograms;
+
+public class loops2 {
+    public static void main(String[]args){
+        for (int i = 1 ; i<=9 ; i++){
+            System.out.println(i);
+            //initialize + evaluation of the condition + body + increment + check + body + increment
+        }
+    }
+    
+}
