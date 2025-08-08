9c63ecb69d5529040746b9261efdaf792082a126
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-08-08 07:21:43 +0530

    use of different loop constructs
    This file includes examples of 'for', 'while', and 'do-while' loops to illustrate control flow in Java.
    Uploaded as part of My100daysCodeChallenge.

---
+++ loops.java
@@ -0,0 +1,17 @@
+package javaPrograms;
+
+public class loops {
+    public static void main(String[]args){
+        int i = 1 ;//initialize , start  i = 1  , i = 2 , i= 3
+
+        while(i<=9){ //condition check i<=9 , 1<=9 , yes , execute , 2<=9 , yes
+            System.out.println(i); // print 1 , 2 ,
+            i= i+1;//print 1++ , 2  , 3
+            //increment
+        }
+        System.out.println("Done");
+        //initialization , evaluation (conition), increment
+
+    }
+    
+}
