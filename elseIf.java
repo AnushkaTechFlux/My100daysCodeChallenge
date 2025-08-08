d8a7b49e354e4d6228cff081cf454c8a7c1ac45f
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-08-08 07:15:37 +0530

    Day 5
    📚 Java else if Statement Example
    This program demonstrates the use of the else if conditional structure in Java. It checks the value of marks and prints the corresponding grade.

---
+++ elseIf.java
@@ -0,0 +1,17 @@
+package javaPrograms;
+
+public class elseIf {
+     public static void main(String[] args) {
+        int n1 = 20;
+        int n2 = 30;
+
+        if(n1 == n2){
+            System.out.println(n1 + " is equal to "+ n2);
+        }
+        else if ( n1>n2){
+            System.out.println(n1+ " is boigger than "+ n2);
+        }else {
+                System.out.println(n2+ " is bigger than "+ n1);
+            }
+        }
+    }
