2a24e3662f7ca5977e85022d0ba0c36c238ecc61
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-08-08 07:25:43 +0530

    print multiplication table of 2
    Java program that prints the multiplication table of 2 using loops.
    Part of My100daysCodeChallenge.

---
+++ tableOfTwo.java
@@ -0,0 +1,9 @@
+package javaPrograms;
+public class tableOfTwo{
+    public static void main(String[]args){
+        int n = 2 ;
+        for (int i = 1 ; i<= 10 ; i++){
+            System.out.println(i*n);
+        }
+    }
+}
